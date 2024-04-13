89a1bdb64e623b721cf7087ce00c2b71526a2a60
wangbhm <160811357+wangbhm@users.noreply.github.com>
2024-04-13 17:29:43 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,7 +6,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_k71v1_64_bsp.mk
+    $(LOCAL_DIR)/twrp_k71v1_64_bsp.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_k71v1_64_bsp-eng
+    twrp_k71v1_64_bsp-eng
